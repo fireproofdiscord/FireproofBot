fd29fcb2af8d86780872cff526f029aae31482fa
Fireproof <fireproofdiscord@gmail.com>
2021-11-18 17:41:11 -0500

    feat: Finish init

--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageActionRow, MessageButton, CategoryChannel } = require("discord.js");
+const { MessageActionRow, MessageButton, CategoryChannel, TextChannel, VoiceChannel } = require("discord.js");
 const https = require("https");
 const { promises } = require("fs");
+const { getOverwrites } = require("../func.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -17,6 +18,8 @@ module.exports = {
 			await interaction.reply("You can only use this command in a guild");
 			return;
 		}
+ 
+		await interaction.deferReply();
 
 		const guildId = interaction.guildId;
 		const bitbucketUser = config.credentials.bitbucket.user;
@@ -25,7 +28,7 @@ module.exports = {
 
 		let repo_name = interaction.options.getString("repo_name");
 
-		if (repo_name === null) {
+		if (!repo_name) {
 			repo_name = guildId;
 		}
 
@@ -34,7 +37,7 @@ module.exports = {
 		try {
 			await git.listRemote([`https://${bitbucketUser}:${bitbucketPass}@bitbucket.org/fireproofdiscord/${slug}`]);
 
-			await interaction.reply({
+			await interaction.followUp({
 				content: "That repository name is taken",
 				ephemeral: true
 			});
@@ -64,7 +67,7 @@ module.exports = {
 							.setStyle("DANGER")
 					);
 
-				confirmation = await interaction.reply({
+				const confirmation = await interaction.followUp({
 					content: "There is already a repository for this guild. Would you like to delete it and make a new one?",
 					ephemeral: true,
 					components: [row],
@@ -80,9 +83,21 @@ module.exports = {
 							ephemeral: true
 						});
 					} else {
-						await promises.rmdir(`../FireproofRepos/${guildId}`, {recursive: true});
+						await promises.rm(`../FireproofRepos/${guildId}`, {recursive: true});
 
-						init(config, slug, guildId, interaction, repo_name, confirmation);
+						await config.Repo.destroy({ 
+							where: {
+								guildId: guildId
+							}
+						});
+
+						await config.Role.destroy({
+							where: {
+								guildId: guildId
+							}
+						});
+
+						init(config, slug, guildId, interaction, repo_name);
 					}
 				});
 			} else {
@@ -92,7 +107,7 @@ module.exports = {
 	}
 }
 
-async function init(config, slug, guildId, interaction, repo_name, message) {
+async function init(config, slug, guildId, interaction, repo_name) {
 	const bitbucketUser = config.credentials.bitbucket.user;
 	const bitbucketPass = config.credentials.bitbucket.pass;
 	const git = config.git;
@@ -118,98 +133,172 @@ async function init(config, slug, guildId, interaction, repo_name, message) {
 
 	const req = https.request(options, async res => {
 		if (res.statusCode === 200) {
-			await git.clone(`https://${bitbucketUser}:${bitbucketPass}@bitbucket.org/fireproofdiscord/${slug}`, guildId);
+			try {
+				await git.clone(`https://${bitbucketUser}:${bitbucketPass}@bitbucket.org/fireproofdiscord/${slug}`, guildId);
 
-			const configFile = {
-				discordCollaborators: [
-					interaction.user.id
-				],
-				bitbucketCollaborators: []
-			}
-
-			await promises.writeFile(`../FireproofRepos/${guildId}/LICENSE`, config.license);
-			await promises.writeFile(`../FireproofRepos/${guildId}/CONFIG.sys.json`, JSON.stringify(configFile, null, 4));
-
-			// get all roles
-			const roles = await interaction.guild.roles.fetch();
-			const roleData = {};
-			const roleCounter = 0;
-
-			for (let role of roles) {
-				// make role objects
-				roleData[roleCounter.toString()] = {
-					name: role.name,
-					color: "default",
-					hoist: role.hoist,
-					mentionable: mentionable,
-					permissions: role.permissions,
-					position: position,
-					boost: role.tags.premiumSubscriberRole
+				const configFile = {
+					discordCollaborators: [
+						interaction.user.id
+					],
+					bitbucketCollaborators: []
 				}
 
+				await promises.writeFile(`../FireproofRepos/${guildId}/LICENSE`, config.license);
+				await promises.writeFile(`../FireproofRepos/${guildId}/config.sys.json`, JSON.stringify(configFile, null, 4));
+
+				// array for all items to be added to repos.sqlite
+				const toAdd = [];
+
+				// get all roles
+				const roles = await interaction.guild.roles.fetch();
+				const roleData = {};
+				let roleCounter = 0;
+
+				// stores role numbers and ids for channel overwrites
+				const overwriteRoles = {};
 				
-			}
+				for (let role of roles) {
+					role = role[1]
 
-			// get all channels
-			const channels = await interaction.guild.channels.fetch();
-			const channelData = {};
+					// next iteration if role is managed by an integration or bot
+					if (role.tags) {
+						if (role.tags.botId || role.tags.integrationId) {
+							continue;
+						}
+					}
 
-			for (let channel of channels) {
-				if (channel instanceof CategoryChannel) {
-					// make category dirs
-					await promises.mkdir(`../FireproofRepos/${guildId}/${channel.name}`);
+					toAdd.push(config.Role.build({
+						number: roleCounter,
+						roleId: role.id || "default",
+						guildId: interaction.guildId
+					}));
 
-					// make category objects
-					channelData[channel.name] = { permissions: {} };
+					let boost;
+					
+					if (role.tags && role.tags.premiumSubscriberRole) {
+						boost = true;
+					} else {
+						boost = false;
+					}
 
-				} else if (channel instanceof TextChannel) {
-					// make text channel objects
+					// make role objects
+					roleData[roleCounter.toString()] = {
+						name: role.name,
+						color: "default",
+						hoist: role.hoist,
+						mentionable: role.mentionable,
+						permissions: role.permissions,
+						position: role.position,
+						boost: boost
+					}
 
+					overwriteRoles[roleCounter.toString()] = role.id;
+
+					roleCounter++;
 				}
-			}
 
-			await git.cwd({ path: `../FireproofRepos/${guildId}` })
-				.checkout(["-b", "main"])
-				.add(".")
-				.commit("First commit")
-				.push("origin", "main");
+				// get all channels
+				const channels = await interaction.guild.channels.fetch();
+				const categoryData = {};
+				const channelData = {};
 
-			await config.Repo.create({ slug: slug, guildId: guildId });
+				for (let channel of channels) {
+					channel = channel[1]
 
-			if (message) {
-				await message.edit(`Repository created at https://bitbucket.org/fireproofdiscord/${slug}`);
-			} else {
-				await interaction.reply(`Repository created at https://bitbucket.org/fireproofdiscord/${slug}`);
+					if (channel.type === "GUILD_CATEGORY") {
+						// make category dirs
+						await promises.mkdir(`../FireproofRepos/${guildId}/${channel.name}`);
+
+						const permissions = getOverwrites(channel, overwriteRoles);
+						
+						// make category objects
+						categoryData[channel.name] = { permissions: permissions };
+					} else if (channel.type === "GUILD_TEXT") {
+						// make text channel objects
+						const permissions = getOverwrites(channel, overwriteRoles);
+
+						let path = channel.name;
+
+						if (channel.parent) {
+							path = `${channel.parent.name}/${channel.name}`;
+						}
+
+						channelData[path] = {
+							type: "text",
+							topic: channel.topic,
+							permissions: permissions,
+							nsfw: channel.nsfw,
+							position: channel.position
+						};
+					} else if (channel.type === "GUILD_VOICE") {
+						//make voice channel objects
+						const permissions = getOverwrites(channel, overwriteRoles);
+						
+						let path = channel.name;
+
+						if (channel.parent) {
+							path = `${channel.parent.name}/${path}`;
+						}
+
+						channelData[path] = {
+							type: "voice",
+							permissions: permissions,
+							bitrate: channel.bitrate,
+							position: channel.position,
+							rtcRegion: channel.rtcRegion,
+							userLimit: channel.userLimit
+						}
+					}
+				}
+
+				await promises.writeFile(`../FireproofRepos/${guildId}/roles.sys.json`, JSON.stringify(roleData, null, 4))
+
+				for (let [path, channel] of Object.entries(categoryData)) {
+					await promises.writeFile(`../FireproofRepos/${guildId}/${path}/category.sys.json`, JSON.stringify(channel, null, 4));
+				}
+
+				for (let [path, channel] of Object.entries(channelData)) {
+					await promises.writeFile(`../FireproofRepos/${guildId}/${path}.json`, JSON.stringify(channel, null, 4));
+				}
+
+				await git.cwd({ path: `../FireproofRepos/${guildId}` })
+					.checkout(["-b", "main"])
+					.add(".")
+					.commit("First commit")
+					.push("origin", "main");
+
+				await config.Repo.create({ slug: slug, guildId: guildId });
+
+				for (let role of toAdd) {
+					await role.save();
+				}
+
+				await interaction.followUp(`Repository created at https://bitbucket.org/fireproofdiscord/${slug}`);
+			} catch (err) {
+				await promises.rm(`../FireproofRepos/${guildId}`, { recursive: true });
+
+				await interaction.followUp({
+					content: "An unexpected error occurred",
+					ephemeral: true
+				});
+	
+				console.error(err);
 			}
 		} else {
-			if (message) {
-				message.edit({
-					content: "An unexpected error occurred",
-					ephemeral: true
-				});
-			} else {
-				interaction.reply({
-					content: "An unexpected error occurred",
-					ephemeral: true
-				});
-			}
+			await interaction.followUp({
+				content: "An unexpected error occurred",
+				ephemeral: true
+			});
 
 			console.log(res.toString());
 		}
 	});
 
-	req.on("error", err => {
-		if (message) {
-			message.edit({
-				content: "An unexpected error occurred",
-				ephemeral: true
-			});
-		} else {
-			interaction.reply({
-				content: "An unexpected error occurred",
-				ephemeral: true
-			});
-		}
+	req.on("error", async err => {
+		await interaction.followUp({
+			content: "An unexpected error occurred",
+			ephemeral: true
+		});
 
 		console.log(err);
 	});
